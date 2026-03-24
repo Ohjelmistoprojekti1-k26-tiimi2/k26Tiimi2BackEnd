e50bf67f3ae7e9b62479aefe8bd048c70996a252
Tanja Vatka <tanja.vatka@hotmail.com>
2026-03-24 16:08:55 +0200

    first commit. Projektin pohjan luonti.

---
+++ src/main/java/hh/ohjelmistoprojekti1/varastonseuranta/VarastonseurantaApplication.java
@@ -0,0 +1,13 @@
+package hh.ohjelmistoprojekti1.varastonseuranta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VarastonseurantaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VarastonseurantaApplication.class, args);
+	}
+
+}
