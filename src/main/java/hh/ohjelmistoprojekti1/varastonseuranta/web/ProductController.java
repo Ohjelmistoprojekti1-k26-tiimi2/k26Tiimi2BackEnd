73bf33aa44b47c4209cb05d5df8fa005f852fdc7
Tanja Vatka <tanja.vatka@hotmail.com>
2026-03-31 16:19:51 +0300

    lisätty index.html tuotteiden listaus toiminto sekä productControlleriin listaus toiminto.

--- src/main/java/hh/ohjelmistoprojekti1/varastonseuranta/web/ProductController.java
+++ src/main/java/hh/ohjelmistoprojekti1/varastonseuranta/web/ProductController.java
@@ -1,13 +1,23 @@
 package hh.ohjelmistoprojekti1.varastonseuranta.web;
 
+import hh.ohjelmistoprojekti1.varastonseuranta.domain.ProductRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ProductController {
 
+  private final ProductRepository productRepository;
+
+  ProductController(ProductRepository productRepository) {
+    this.productRepository = productRepository;
+  }
+
   @GetMapping("/")
-  public String main() {
+  public String main(Model model) {
+
+    model.addAttribute("products", productRepository.findAll());
     return "index"; // index.html
   }
 
