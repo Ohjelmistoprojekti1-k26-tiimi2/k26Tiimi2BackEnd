80d599d8698dd400a33db6c3e9afec5d538cf5b8
= <=>
2026-04-01 10:40:51 +0300

    controlleriin lisätty tuotteen lisäys- ja tallennusmetodit

--- src/main/java/hh/ohjelmistoprojekti1/varastonseuranta/web/ProductController.java
+++ src/main/java/hh/ohjelmistoprojekti1/varastonseuranta/web/ProductController.java
@@ -1,9 +1,12 @@
 package hh.ohjelmistoprojekti1.varastonseuranta.web;
 
+import hh.ohjelmistoprojekti1.varastonseuranta.domain.Product;
 import hh.ohjelmistoprojekti1.varastonseuranta.domain.ProductRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ProductController {
@@ -14,11 +17,25 @@ public class ProductController {
     this.productRepository = productRepository;
   }
 
-  @GetMapping("/")
+  @GetMapping("/index")
   public String main(Model model) {
 
     model.addAttribute("products", productRepository.findAll());
     return "index"; // index.html
+  }
+
+  // uuden tuotteen lisäys (lomake)
+  @GetMapping("/addproduct")
+  public String addProduct(Model model) {
+    model.addAttribute("product", new Product());
+    return "addproduct"; // addproduct.html
+  }
+
+  // tuotteen tallennus
+  @PostMapping("/saveproduct")
+  public String save(@ModelAttribute Product product) {
+    productRepository.save(product);
+    return "redirect:../index"; // index.html
   }
 
 }
