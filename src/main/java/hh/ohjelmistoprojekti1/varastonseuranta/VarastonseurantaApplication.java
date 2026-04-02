e4be796dbea40d58b7301b410ac358f782dcf5d1
= <=>
2026-04-02 16:41:43 +0300

    testidataa muokattu lisää

--- src/main/java/hh/ohjelmistoprojekti1/varastonseuranta/VarastonseurantaApplication.java
+++ src/main/java/hh/ohjelmistoprojekti1/varastonseuranta/VarastonseurantaApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.boot.CommandLineRunner;
 
+import hh.ohjelmistoprojekti1.varastonseuranta.domain.Manufacturer;
+import hh.ohjelmistoprojekti1.varastonseuranta.domain.ManufacturerRepository;
 import hh.ohjelmistoprojekti1.varastonseuranta.domain.Product;
 import hh.ohjelmistoprojekti1.varastonseuranta.domain.ProductRepository;
 
@@ -15,8 +17,10 @@ public class VarastonseurantaApplication {
 	}
 
 	@Bean
-	public CommandLineRunner demo(ProductRepository productRepo) {
+	public CommandLineRunner demo(ProductRepository productRepo, ManufacturerRepository manufacturerRepo) {
 		return (args) -> {
+
+			// Manufacturer manufacturer1
 
 			productRepo.save(
 					new Product("Pikku mussukan sadetakki", "Koiran sadetakki", "Pinkki", "X-Large", 19.99, "Rukka"));
