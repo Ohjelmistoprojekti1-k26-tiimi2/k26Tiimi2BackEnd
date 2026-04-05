618a6c33dfae4fbb0803efae5fec53f80ee01eda
= <=>
2026-04-05 12:54:32 +0300

    lisätty testidataa valmistajista

--- src/main/java/hh/ohjelmistoprojekti1/varastonseuranta/VarastonseurantaApplication.java
+++ src/main/java/hh/ohjelmistoprojekti1/varastonseuranta/VarastonseurantaApplication.java
@@ -5,26 +5,37 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.boot.CommandLineRunner;
 
+import hh.ohjelmistoprojekti1.varastonseuranta.domain.Manufacturer;
 import hh.ohjelmistoprojekti1.varastonseuranta.domain.ManufacturerRepository;
 import hh.ohjelmistoprojekti1.varastonseuranta.domain.Product;
 import hh.ohjelmistoprojekti1.varastonseuranta.domain.ProductRepository;
 
 @SpringBootApplication
 public class VarastonseurantaApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(VarastonseurantaApplication.class, args);
 	}
 
 	@Bean
-	public CommandLineRunner demo(ProductRepository productRepo, ManufacturerRepository manufacturerRepo) {
+	public CommandLineRunner demo(ProductRepository productRepo, ManufacturerRepository manufacturerRepository) {
 		return (args) -> {
 
-			// Manufacturer manufacturer1
+			// testidataa valmistajsta
+			Manufacturer manufacturer1 = new Manufacturer("Rukka");
+			manufacturerRepository.save(manufacturer1);
+			Manufacturer manufacturer2 = new Manufacturer("NORDICDOG");
+			manufacturerRepository.save(manufacturer2);
+			Manufacturer manufacturer3 = new Manufacturer("Ruffwear");
+			manufacturerRepository.save(manufacturer3);
 
+			// testidataa tuotteista
 			productRepo.save(
-					new Product("Pikku mussukan sadetakki", "Koiran sadetakki", "Pinkki", "X-Large", 19.99, "Rukka"));
+					new Product("Pikku mussukan sadetakki", "Koiran sadetakki", "Pinkki", "X-Large", 19.99,
+							manufacturer1));
 			productRepo
-					.save(new Product("Pikku mussukan tohvelit", "Koiran tohvelit", "Musta", "X-Small", 25, "Rukka"));
+					.save(new Product("Pikku mussukan tohvelit", "Koiran tohvelit", "Musta", "X-Small", 25,
+							manufacturer3));
 		};
 
 	}
