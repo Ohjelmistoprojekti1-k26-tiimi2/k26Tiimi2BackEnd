1b750027171c459a25b727b9863bfa003384f94d
archiartt <inkalanarttu@gmail.com>
2026-04-14 12:50:21 +0300

    Poistettu turha importti RestProductControllerista.

--- src/main/java/hh/ohjelmistoprojekti1/varastonseuranta/web/RestProductController.java
+++ src/main/java/hh/ohjelmistoprojekti1/varastonseuranta/web/RestProductController.java
@@ -1,6 +1,5 @@
 package hh.ohjelmistoprojekti1.varastonseuranta.web;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
