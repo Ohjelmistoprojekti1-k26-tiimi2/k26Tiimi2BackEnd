96203702623afc90051653bce80e29c36768a94b
= <=>
2026-04-16 16:59:13 +0300

    lisätty metodit ruuan/vaatteen/lelun formin näkymiselle + tallennukselle

--- src/main/java/hh/ohjelmistoprojekti1/varastonseuranta/web/ProductController.java
+++ src/main/java/hh/ohjelmistoprojekti1/varastonseuranta/web/ProductController.java
@@ -1,8 +1,12 @@
 package hh.ohjelmistoprojekti1.varastonseuranta.web;
 
+import hh.ohjelmistoprojekti1.varastonseuranta.domain.Clothing;
+import hh.ohjelmistoprojekti1.varastonseuranta.domain.Food;
 import hh.ohjelmistoprojekti1.varastonseuranta.domain.ManufacturerRepository;
 import hh.ohjelmistoprojekti1.varastonseuranta.domain.Product;
 import hh.ohjelmistoprojekti1.varastonseuranta.domain.ProductRepository;
+import hh.ohjelmistoprojekti1.varastonseuranta.domain.Size;
+import hh.ohjelmistoprojekti1.varastonseuranta.domain.Toy;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -33,7 +37,7 @@ public class ProductController {
   // uuden tuotteen lisäys (lomake)
   @GetMapping("/addproduct")
   public String addProduct(Model model) {
-    model.addAttribute("productForm", new ProductForm());
+    // model.addAttribute("productForm", new ProductForm());
 
     // haetan kaikki valmistajat tietokannasta
     model.addAttribute("manufacturers", manufacturerRepository.findAll());
@@ -45,6 +49,63 @@ public class ProductController {
   public String save(@ModelAttribute Product product) {
     productRepository.save(product);
     return "redirect:/index"; // index.html
+  }
+
+  // haetaan lelun lisäykseen formipohja
+  @GetMapping("/addtoy")
+  public String addToy(Model model) {
+    model.addAttribute("toy", new Toy());
+    model.addAttribute("manufacturers", manufacturerRepository.findAll());
+    return "addtoy"; // addtoy.html
+  }
+
+  // lelun tallennus (from)
+  @PostMapping("/savetoy")
+  public String saveToy(@ModelAttribute Toy toy) {
+    // asetetaan tuotteen tyypiksi lelu
+    toy.setProductType("Lelu");
+
+    productRepository.save(toy);
+    return "redirect:/index";
+  }
+
+  // haetaan ruuan lisäykseen formipohja
+  @GetMapping("/addfood")
+  public String addFood(Model model) {
+    model.addAttribute("food", new Food());
+    model.addAttribute("manufacturers", manufacturerRepository.findAll());
+    return "addfood"; // addfood.html
+  }
+
+  // ruuan tallennus (from)
+  @PostMapping("/savefood")
+  public String saveFood(@ModelAttribute Food food) {
+    // asetetaan tuotteen tyypiksi ruoka
+    food.setProductType("Ruoka");
+
+    productRepository.save(food);
+    return "redirect:/index";
+  }
+
+  // haetaan vaatteen lisäykseen formipohja
+  @GetMapping("/addclothing")
+  public String addClothing(Model model) {
+    model.addAttribute("clothing", new Clothing());
+    // haetaan valmistajatiedot
+    model.addAttribute("manufacturers", manufacturerRepository.findAll());
+    // haetaan kokotiedot
+    model.addAttribute("sizes", Size.values());
+    return "addclothing"; // addclothing.html
+  }
+
+  // vaatteen tallennus (from)
+  @PostMapping("/saveclothing")
+  public String saveClothing(@ModelAttribute Clothing clothing) {
+    // asetetaan tuotteen tyypiksi vaate
+    clothing.setProductType("Vaate");
+
+    productRepository.save(clothing);
+    return "redirect:/index";
   }
 
   // tuotteen muokkaus - Edit product
