b798230adcfbd47b323ea4081927f231ac7d4952
Tanja Vatka <tanja.vatka@hotmail.com>
2026-04-21 13:55:23 +0300

    ei muutoksia

--- src/main/java/hh/ohjelmistoprojekti1/varastonseuranta/web/RestProductController.java
+++ src/main/java/hh/ohjelmistoprojekti1/varastonseuranta/web/RestProductController.java
@@ -1,4 +1,5 @@
 package hh.ohjelmistoprojekti1.varastonseuranta.web;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -13,43 +14,45 @@ import org.springframework.web.bind.annotation.RestController;
 import hh.ohjelmistoprojekti1.varastonseuranta.domain.Product;
 import hh.ohjelmistoprojekti1.varastonseuranta.domain.ProductRepository;
 
-
 @RestController
 @RequestMapping("/products")
-//KAIKKI TÄMÄN CONTROLLERIN ENDPOINTIT ALKAVAT TÄLLÄ POLULLA
+// KAIKKI TÄMÄN CONTROLLERIN ENDPOINTIT ALKAVAT TÄLLÄ POLULLA
 public class RestProductController {
-    //REPOSITORIO JONKA KAUTTA HAETAAN JA TALLENNETAAN DATAA TIETOKANTAAN
+    // REPOSITORIO JONKA KAUTTA HAETAAN JA TALLENNETAAN DATAA TIETOKANTAAN
     private final ProductRepository productRepository;
 
     public RestProductController(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
-// HAETAAN KAKKI TUOTTEET TIETOKANNASTA
+
+    // HAETAAN KAKKI TUOTTEET TIETOKANNASTA
     @GetMapping
     public List<Product> getProducts() {
         return (List<Product>) productRepository.findAll();
     }
-    //HAKEE YHDEN TUOTTEEN ID:N PERUSTEELLA
+
+    // HAKEE YHDEN TUOTTEEN ID:N PERUSTEELLA
     @GetMapping("/{id}")
-    public Optional<Product> getProductById(@PathVariable("id") Long productId){
+    public Optional<Product> getProductById(@PathVariable("id") Long productId) {
         return productRepository.findById(productId);
     }
-    //LISÄÄ UUDEN TUOTTEEN
+
+    // LISÄÄ UUDEN TUOTTEEN
     @PostMapping
     public Product newProduct(Product newProduct) {
         return productRepository.save(newProduct);
     }
 
-    //POISTAA TUOTTEEN ID:N PERUSTEELLA
+    // POISTAA TUOTTEEN ID:N PERUSTEELLA
     @DeleteMapping("/{id}")
     public void deleteProduct(@PathVariable("id") Long productId) {
         productRepository.deleteById(productId);
     }
 
-    //PÄIVITTÄÄ OLEMASSA OLEVAN TUOTTEEN
+    // PÄIVITTÄÄ OLEMASSA OLEVAN TUOTTEEN
     @PutMapping("/{id}")
     public Product editProduct(Product editedProduct,
-                               @PathVariable("id") Long productId) {
+            @PathVariable("id") Long productId) {
 
         // ASETETAAN OIKEA ID, JOTTA OIKEA TUOTE PÄIVITTYY
         editedProduct.setProductId(productId);
