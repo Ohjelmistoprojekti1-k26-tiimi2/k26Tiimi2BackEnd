637cc3080f7129df5be9a205b4621e0168522c06
= <=>
2026-04-26 18:01:04 +0300

    lisätty valmistajatietoja + ruokatuoteista testidataa

--- src/main/java/hh/ohjelmistoprojekti1/varastonseuranta/VarastonseurantaApplication.java
+++ src/main/java/hh/ohjelmistoprojekti1/varastonseuranta/VarastonseurantaApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.boot.CommandLineRunner;
 
+import hh.ohjelmistoprojekti1.varastonseuranta.domain.Food;
+import hh.ohjelmistoprojekti1.varastonseuranta.domain.FoodRepository;
 import hh.ohjelmistoprojekti1.varastonseuranta.domain.Manufacturer;
 import hh.ohjelmistoprojekti1.varastonseuranta.domain.ManufacturerRepository;
 import hh.ohjelmistoprojekti1.varastonseuranta.domain.Product;
@@ -18,7 +20,8 @@ public class VarastonseurantaApplication {
 	}
 
 	@Bean
-	public CommandLineRunner demo(ProductRepository productRepo, ManufacturerRepository manufacturerRepository) {
+	public CommandLineRunner demo(ProductRepository productRepo, ManufacturerRepository manufacturerRepository,
+			FoodRepository foodRepository) {
 		return (args) -> {
 
 			// testidataa valmistajsta
@@ -29,12 +32,30 @@ public class VarastonseurantaApplication {
 			Manufacturer manufacturer3 = new Manufacturer("Ruffwear");
 			manufacturerRepository.save(manufacturer3);
 
-			// testidataa tuotteista
-			productRepo.save(
-					new Product("Pikku mussukan sadetakki", 19.99, manufacturer1));
+			// ruokatuotteiden valmistajat
+			Manufacturer manufacturer4 = new Manufacturer("Brit Care");
+			manufacturerRepository.save(manufacturer4);
+			Manufacturer manufacturer5 = new Manufacturer("Hau Hau Champion");
+			manufacturerRepository.save(manufacturer5);
+			Manufacturer manufacturer6 = new Manufacturer("PrimaDog");
+			manufacturerRepository.save(manufacturer6);
 
-			productRepo.save(
-					new Product("Pikku mussukan tohvelit", 25, manufacturer3));
+			// food -data
+			Food food1 = new Food("Kananappulat", 29.95, manufacturer4, "kuivaruoka", "aikuinen");
+			foodRepository.save(food1);
+			Food food2 = new Food("Koiranmakkara, nauta", 5.95, manufacturer5, "märkäruoka", "aikuinen");
+			foodRepository.save(food2);
+			Food food3 = new Food("Lammasnappulat", 20.99, manufacturer6, "kuivaruoka", "seniori");
+			foodRepository.save(food3);
+			Food food4 = new Food("Lammasta ja riisiä, pentunappulat", 35.95, manufacturer4, "kuivaruoka", "juniori");
+			foodRepository.save(food4);
+
+			// testidataa tuotteista
+			// productRepo.save(
+			// new Product("Pikku mussukan sadetakki", 19.99, manufacturer1));
+
+			// productRepo.save(
+			// new Product("Pikku mussukan tohvelit", 25, manufacturer3));
 
 		};
 
