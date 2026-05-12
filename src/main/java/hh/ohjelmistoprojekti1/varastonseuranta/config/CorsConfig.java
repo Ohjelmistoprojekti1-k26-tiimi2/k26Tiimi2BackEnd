05ddfce45a410efc3548a0c9e9b583aa047e6823
= <=>
2026-05-12 13:28:42 +0300

    Lisätty frontin deployatty url

--- src/main/java/hh/ohjelmistoprojekti1/varastonseuranta/config/CorsConfig.java
+++ src/main/java/hh/ohjelmistoprojekti1/varastonseuranta/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOriginPatterns("http://localhost:5173")
+                .allowedOriginPatterns("http://localhost:5173", "https://ohjelmistoprojekti1-k26-tiimi2.github.io")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
